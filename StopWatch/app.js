f62d4b5cf954133b687add1cf164a951c8cfc0f3
Maksim Mukovin <mukovin.maksim@lcw.io>
2019-01-16 15:49:12 +0300

    update StopWatch

--- StopWatch/app.js
+++ StopWatch/app.js
@@ -1,17 +1,21 @@
 // Запрет спящего режима
-// var noSleep =  new  NoSleep ();
-// document.addEventListener('click', function enableNoSleep() {
-//   document.removeEventListener('click', enableNoSleep, false);
-//   noSleep.enable();
-// }, false);
-// === ===
+var noSleep =  new  NoSleep (),
+    timerNS;
+
+function enableNoSleep() {
+  noSleep.enable();
+	setTimeout(function(){noSleep.disable();}, 100);
+  timerNS = setTimeout(enableNoSleep, 30000);
+};
+//
 
 var minute = 0,
     second = 0,
     time = `${minute}:${second}`,
     timerId,
+    status = 0,
     round = 0,
-    status = 0;
+    _distance = 0;
 
 function timerFn(){
   second++
@@ -32,15 +36,22 @@ function timerFn(){
 // === События ===
 btnStart.addEventListener('click', function(){
   status = 1; clearTimeout(timerId); timerFn();
+  enableNoSleep()
 });
 
 btnStop.addEventListener('click', function(){
   status = 0; clearTimeout(timerId);
+  noSleep.disable();
+  clearTimeout(timerNS);
 });
 
 btnRound.addEventListener('click', function(){
   round++;
-  roundKpi.innerHTML = round;
+  rounds.innerHTML = round;
+  _distance = _distance + parseInt(distance.value)
+  console.log(distance.value);
+
+  totalDistance.innerHTML = _distance;
 
   var row = document.createElement('div');
   row.appendChild(document.createTextNode(`Круг:${round}, Время:${time}`));
---
+++ noSleep/app.js
@@ -0,0 +1,24 @@
+var noSleep =  new  NoSleep (),
+    timerId;
+
+function enableNoSleep() {
+  noSleep.enable();
+	var date = new Date();
+  var check = document.createElement("div");
+  check.appendChild(document.createTextNode(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`))
+  document.body.appendChild(check)
+  setTimeout(function(){noSleep.disable();}, 100);
+  timerId = setTimeout(enableNoSleep, 30000);
+// 100 20000 работает
+// 1 10000 работает
+// 1 20000 нет
+// 10 20000 работает но не на грани срыва
+// 100 30000 работает но не на грани срыва
+};
+
+btnStart.addEventListener('click', enableNoSleep)
+
+btnStop.addEventListener('click', function(){
+  noSleep.disable();
+  clearTimeout(timerId);
+})
