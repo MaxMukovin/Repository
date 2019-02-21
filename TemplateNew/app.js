c89d1ed19f66e704ea53e7a122244dbed1ffe6e8
Maksim Mukovin <mukovin.maksim@lcw.io>
2019-02-21 17:58:01 +0300

    add Text to word, templateNew

--- TemplateNew/app.js
+++ TemplateNew/app.js
@@ -1,35 +1,55 @@
-function get_cookie ( cookie_name )
-{
-  var results = document.cookie.match ( '(^|;) ?' + cookie_name + '=([^;]*)(;|$)' );
+text.value = "This “Constitution” defines rules and processes for the governance and operations of an organization. The “Ratifiers” are adopting these rules as the formal authority structure for the “Organization” specified upon the Constitution’s adoption, which may be an entire entity or a part of one that the Ratifiers have authority to govern and run. The Ratifiers and anyone else who agrees to take part in the governance and operations of the Organization (its “Partners”) may rely upon the authorities granted by this Constitution, and also agree to be bound by its duties and constraints";
 
-  if ( results )
-  return ( unescape ( results[2] ) );
-  else
-  return null;
+btnConfirm.onclick = function() {
+  while (table.children.length > 0) {
+    table.removeChild(table.children[0])
+  }
+  var data = text.value.toLowerCase().replace(/\(|\)|\.|\,/g, '');
+
+
+  data = data.split(" ").map(String).sort();
+
+  function unique(arr) {
+    var obj = {};
+
+    for (var i = 0; i < arr.length; i++) {
+      var str = arr[i];
+      obj[str] = true; // запомнить строку в виде свойства объекта
+    }
+
+    return Object.keys(obj); // или собрать ключи перебором для IE8-
+  }
+
+  data = unique(data)
+
+  var color1 = "#fff",
+      color2 = "#f5f5f5",
+      currentColor = color1,
+      counterColor = 0,
+      check = "a";
+  console.log(data);
+  // var table = container.appendChild(document.createElement("table"));
+  for (var i = 0; i < data.length; i++) {
+    var tr = table.appendChild(document.createElement("tr"));
+    var td1 = tr.appendChild(document.createElement("td"));
+    var td2 = tr.appendChild(document.createElement("td"));
+    td1.innerHTML = data[i];
+    td1.className = "notranslate";
+    td2.innerHTML = data[i];
+
+
+    if (data[i][0] != check) {
+      counterColor++;
+      if (counterColor %2 == 0) {
+        currentColor = color1;
+      } else {
+        currentColor = color2;
+      }
+      check = data[i][0];
+    }
+
+    tr.style.background = currentColor;
+  }
+  // document.body.children[0].removeChild(text)
+
 }
-
-var array = [];
-
-for (var i = 0; i < 20; i++) {
-  array.push(i)
-}
-
-console.log(array);
-
-document.cookie = `array = ${array}`;
-document.cookie = 'number = 123';
-document.cookie = "username = User1";
-document.cookie = "username = User123132123";
-// number = 123;
-
-// Получение массива
-var x = get_cookie("array").split(",").map(Number);
-
-// var newArray = x.split(",").map(Number);
-
-
-
-
-// console.log(document.cookie);
-console.log(x);
-// console.log(newArray);
